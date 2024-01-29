5d92fab2e3dc722cc1bc88403e4e1501fe603e74
GuillaumeRhodes <rhodes.guillaume46@gmail.com>
2024-01-29 18:20:06 +0100

    first react app finished

--- J03/ReactProject/src/App.jsx
+++ J03/ReactProject/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// On va créer notre premier composant React. Un composant est simplement une fonction. Mais une fonction qui renvoit du "HTML".
+const App = () => {
+  const tableau = ["un", "deux", "trois"];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // À l'instar de notre fonction configureElement (dans notre projet natif), on retrouve une fonction similaire avec React.
+    // React.createElement(
+    //   'div',
+    //   { className: 'container' },
+    //   ['Hello World']
+    // )
+
+    // Mais ce n'est pas la méthode la plus propre ni la plus simple, donc React nous donne à disposition un outil appelé, JSX.
+    // Le JSX, c'est simplement une surcouche de JS. Il permet de faire tout ce que fait le JS, mais avec quelque chose en plus, comme par exemple le fait de pouvoir écrire directement du HTML (quelque de TRES similaire) dans nos fichiers .js (.jsx).
+    <Container>
+      <div className="container">Hello World</div>
+      {/* On peut appeler nos composants sous forme de fonction. */}
+      {/* {List()} */}
+      {/* On peut plutôt, pour pas dénoter du reste du code (html/jsx), l'écrire sous forme de balise. */}
+      {/* Pour rajouter des propriétés à mon composant, je vais tout simplement lui rajouter comme des attribues en html. */}
+      <List numbers={tableau} />
+    </Container>
+  )
+};
+
+// Maintenant je vais créer un deuxième composant.
+// Un composant peut recevoir des propriétés (properties/props). Ces propriétés vont être réunis sous un même objet reçu en paramètre de ma fonction (composant).
+const List = (props) => {
+  console.log("List", props)
+
+  // Si j'écris une variable/expression dans des balises HTML, je dois l'encapsuler entre des accolades {}.
+  // Si on écrit un tableau d'élément JSX, on doit rajouter un attribue qui s'appelle key, et qui doit être unique. Il agit comme un id, et est utilisé seulement par React, pour nous c'est transparant (on s'en sert pas).
+  const result = props.numbers.map(element => <li key={element}>{element}</li>);
+
+  // Je n'oublie pas de return mon result, qui est en fait mon html (un tableau de li)
+  return <ul>{result}</ul>;
+}
+
+// Je crée un nouveau composant, qui sera simplement le container de mes autres composants dans App.
+// La particularité de ce composant, c'est qu'il va contenir d'autres composants mais on ne sait pas encore ni combien ni lesquels.
+// Pour ce faire, on va se servir de la propriété children qui est créée automatiquement lorsqu'on met des composants entre les balises (<Container>Des chose</Container).
+const Container = props => {
+  console.log("Container", props)
+  return (
+    <div className="container">
+      {props.children}
+    </div>
   )
 }
 
-export default App
+// L'export va permettre de pouvoir récupérer le composant/fonction depuis un autre fichier (dans notre cas, dans main.jsx).
+// L'export permet de faire un import.
+// On ne peut faire qu'un seul export default par fichier.
+export default App;
--- J03/ReactProject/src/main.jsx
+++ J03/ReactProject/src/main.jsx
@@ -1,10 +1,15 @@
+// import, c'est une méthode pour récupérer une information depuis un autre fichier.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 
+// ReactDOM c'est l'objet qui nous permet de charger l'application dans notre DOM (le index.html).
+// Pour se faire, on utile la méthode createRoot, qui prend en argument l'élément dans lequel on va mettre l'application (dans notre cas, c'est la div avec l'id root).
+// Cette même méthode nous donne accès à une autre méthode qui s'appelle render, render va s'occuper de charger le résultat, à render on donne en arguement l'application à charger (dans notre cas, App).
+// On ignorera pous le moment le React.StrictMode (on le commente pour le moment).
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>,
 )
